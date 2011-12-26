9b77db9a53343124c54695c8b302674f74114c34
Roman Starkov <sourcecode@roman.st>
2011-12-26 20:30:11 +0000

    Can now render basic images.

---
+++ Util.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace TankIconMaker
+{
+    static class Ut
+    {
+        public static IEnumerable<Tuple<int, string[]>> ReadCsvLines(string filename)
+        {
+            int num = 0;
+            foreach (var line in File.ReadLines(filename))
+            {
+                num++;
+                var fields = parseCsvLine(line);
+                if (fields == null)
+                    throw new Exception(string.Format("Couldn't parse line {0}.", num));
+                yield return Tuple.Create(num, fields);
+            }
+        }
+
+        private static string[] parseCsvLine(string line)
+        {
+            var fields = Regex.Matches(line, @"(^|(?<=,)) *(?<quote>""?)(("""")?[^""]*?)*?\k<quote> *($|(?=,))").Cast<Match>().Select(m => m.Value).ToArray();
+            if (line != string.Join(",", fields))
+                return null;
+            return fields.Select(f => f.Contains('"') ? Regex.Replace(f, @"^ *""(.*)"" *$", "$1").Replace(@"""""", @"""") : f).ToArray();
+        }
+
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+    }
+
+    static class ExtensionMethods
+    {
+        public static bool EqualsNoCase(this string string1, string string2)
+        {
+            return StringComparer.OrdinalIgnoreCase.Equals(string1, string2);
+        }
+    }
+}
