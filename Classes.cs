14b0abfe03e3a24c7244f663c3f81ee22823a99a
rstarkov <rstarkov@localhost>
2011-12-26 20:30:11 +0000

    Can now render basic images.

---
+++ Classes.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace TankIconMaker
+{
+    class Tank
+    {
+        public string SystemId { get; protected set; }
+        public Country Country { get; protected set; }
+        public int Tier { get; protected set; }
+        public Class Class { get; protected set; }
+        public Category Category { get; protected set; }
+
+        private Dictionary<string, string> _extras;
+
+        protected Tank() { }
+
+        public Tank(TankData tank, IEnumerable<KeyValuePair<string, string>> extras)
+        {
+            SystemId = tank.SystemId;
+            Country = tank.Country;
+            Tier = tank.Tier;
+            Class = tank.Class;
+            Category = tank.Category;
+            _extras = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            foreach (var extra in extras)
+                _extras.Add(extra.Key, extra.Value);
+        }
+
+        public string this[string name]
+        {
+            get
+            {
+                string result;
+                if (!_extras.TryGetValue(name, out result))
+                    return null;
+                return result;
+            }
+        }
+    }
+
+    class TankData : Tank
+    {
+        public TankData(string[] fields)
+        {
+            if (fields.Length < 5)
+                throw new Exception(string.Format("Expected at least 5 fields"));
+
+            SystemId = fields[0];
+
+            switch (fields[1])
+            {
+                case "ussr": Country = Country.USSR; break;
+                case "germany": Country = Country.Germany; break;
+                case "usa": Country = Country.USA; break;
+                case "china": Country = Country.China; break;
+                case "france": Country = Country.France; break;
+                default: throw new Exception(string.Format("Unrecognized country: \"{0}\"", fields[1]));
+            }
+
+            int tier;
+            if (!int.TryParse(fields[2], out tier))
+                throw new Exception(string.Format("The tier field was not a whole number: \"{0}\"", fields[2]));
+            if (tier < 1 || tier > 10)
+                throw new Exception("Tank tier is not in the 1..10 range");
+            Tier = tier;
+
+            switch (fields[3])
+            {
+                case "light": Class = Class.Light; break;
+                case "medium": Class = Class.Medium; break;
+                case "heavy": Class = Class.Heavy; break;
+                case "destroyer": Class = Class.Destroyer; break;
+                case "artillery": Class = Class.Artillery; break;
+                default: throw new Exception(string.Format("Unrecognized class: \"{0}\"", fields[3]));
+            }
+
+            switch (fields[4])
+            {
+                case "normal": Category = Category.Normal; break;
+                case "premium": Category = Category.Premium; break;
+                case "special": Category = Category.Special; break;
+                default: throw new Exception(string.Format("Unrecognized category: \"{0}\"", fields[4]));
+            }
+        }
+    }
+
+    class DataFileBuiltIn
+    {
+        public string Author { get; private set; }
+        public Version GameVersion { get; private set; }
+        public int FileVersion { get; private set; }
+
+        public IList<TankData> Data { get; private set; }
+
+        public DataFileBuiltIn(string author, Version gameVersion, int fileVersion, string filename)
+        {
+            Author = author;
+            GameVersion = gameVersion;
+            FileVersion = fileVersion;
+
+            Data = Ut.ReadCsvLines(filename).Select(lp =>
+            {
+                try { return new TankData(lp.Item2); }
+                catch (Exception e) { throw new Exception(e.Message + " at line " + lp.Item1); }
+            }).ToList().AsReadOnly();
+        }
+    }
+
+    class DataFileExtra
+    {
+        public string Name { get; private set; }
+        public string Language { get; private set; }
+        public string Author { get; private set; }
+        public Version GameVersion { get; private set; }
+        public int FileVersion { get; private set; }
+
+        public IList<ExtraData> Data { get; private set; }
+
+        public DataFileExtra(string name, string language, string author, Version gameVersion, int fileVersion, string filename)
+        {
+            Name = name;
+            Language = language;
+            Author = author;
+            GameVersion = gameVersion;
+            FileVersion = fileVersion;
+
+            Data = Ut.ReadCsvLines(filename).Select(lp =>
+            {
+                try { return new ExtraData(lp.Item2); }
+                catch (Exception e) { throw new Exception(e.Message + " at line " + lp.Item1); }
+            }).ToList().AsReadOnly();
+        }
+    }
+
+    class ExtraData
+    {
+        public string TankSystemId { get; private set; }
+        public string Value { get; private set; }
+
+        public ExtraData(string[] fields)
+        {
+            if (fields.Length < 2)
+                throw new Exception(string.Format("Expected at least 2 fields"));
+            TankSystemId = fields[0];
+            Value = fields[1];
+        }
+    }
+
+    enum Country
+    {
+        USSR,
+        Germany,
+        USA,
+        France,
+        China,
+    }
+
+    enum Class
+    {
+        Light,
+        Medium,
+        Heavy,
+        Destroyer,
+        Artillery,
+    }
+
+    enum Category
+    {
+        Normal,
+        Premium,
+        Special,
+    }
+
+    abstract class IconMaker
+    {
+        public abstract string Name { get; }
+        public abstract string Author { get; }
+        public abstract int Version { get; }
+        public abstract BytesBitmap DrawTank(Tank tank);
+
+        protected static BytesBitmap NewBitmap()
+        {
+            return new BytesBitmap(80, 24, PixelFormat.Format32bppArgb);
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,15 +12,132 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace TankIconMaker
 {
     public partial class MainWindow : Window
     {
+        private string _exePath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        private List<DataFileBuiltIn> _builtin = new List<DataFileBuiltIn>();
+        private List<DataFileExtra> _extra = new List<DataFileExtra>();
+
         public MainWindow()
         {
             InitializeComponent();
+            ReloadData();
+
+            if (File.Exists(Path.Combine(_exePath, "background.jpg")))
+                outerGrid.Background = new ImageBrush
+                {
+                    ImageSource = new BitmapImage(new Uri(Path.Combine(_exePath, "background.jpg"))),
+                    Stretch = Stretch.UniformToFill,
+                };
+        }
+
+        private void ReloadData()
+        {
+            _builtin.Clear();
+            _extra.Clear();
+
+            foreach (var fi in new DirectoryInfo(_exePath).GetFiles("Data-*.csv"))
+            {
+                var parts = fi.Name.Substring(0, fi.Name.Length - 4).Split('-');
+                var partsr = parts.Reverse().ToArray();
+
+                if (parts.Length < 5 || parts.Length > 6)
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has the wrong number of filename parts.", fi.Name);
+                    continue;
+                }
+                if (parts[1].EqualsNoCase("BuiltIn") && parts.Length != 5)
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has too many filename parts for a BuiltIn data file.", fi.Name);
+                    continue;
+                }
+                if (parts.Length == 5 && !parts[1].EqualsNoCase("BuiltIn"))
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has too few filename parts for a non-BuiltIn data file.", fi.Name);
+                    continue;
+                }
+
+                string author = partsr[2].Trim();
+                if (author.Length == 0)
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has an empty author part in the filename.", fi.Name);
+                    continue;
+                }
+
+                Version gameVersion;
+                if (!Version.TryParse(partsr[1], out gameVersion))
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has an unparseable game version part in the filename: \"{1}\".", fi.Name, partsr[1]);
+                    continue;
+                }
+
+                int fileVersion;
+                if (!int.TryParse(partsr[0], out fileVersion))
+                {
+                    Console.WriteLine("Skipping \"{0}\" because it has an unparseable file version part in the filename: \"{1}\".", fi.Name, partsr[0]);
+                    continue;
+                }
+
+                if (parts.Length == 5)
+                    _builtin.Add(new DataFileBuiltIn(author, gameVersion, fileVersion, fi.FullName));
+                else
+                {
+                    string extraName = parts[1].Trim();
+                    if (extraName.Length == 0)
+                    {
+                        Console.WriteLine("Skipping \"{0}\" because it has an empty property name part in the filename.", fi.Name);
+                        continue;
+                    }
+
+                    string languageName = parts[2].Trim();
+                    if (languageName.Length != 2)
+                    {
+                        Console.WriteLine("Skipping \"{0}\" because its language name part in the filename is a 2 letter long language code.", fi.Name);
+                        continue;
+                    }
+
+                    _extra.Add(new DataFileExtra(extraName, languageName, author, gameVersion, fileVersion, fi.FullName));
+                }
+            }
+
+            tankIcons.Children.Clear();
+            var maker = new Test1Maker();
+            foreach (var tank in DistinctTanks(EnumTanks()))
+            {
+                var bmp = maker.DrawTank(tank);
+                var handle = bmp.Bitmap.GetHbitmap();
+                var bmpWpf = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(handle, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+                tankIcons.Children.Add(new Image
+                {
+                    Source = bmpWpf,
+                    Width = 80, Height = 24,
+                    SnapsToDevicePixels = true,
+                    Margin = new Thickness { Right = 15 },
+                    ToolTip = tank.SystemId + (tank["OfficialName"] == null ? "" : (" (" + tank["OfficialName"] + ")")),
+                });
+
+                Ut.DeleteObject(handle);
+                GC.KeepAlive(bmp);
+            }
+        }
+
+        private IEnumerable<Tank> EnumTanks()
+        {
+            return _builtin.First().Data.Select(tank => new Tank(
+                tank,
+                new[] { new KeyValuePair<string, string>(_extra[1].Name, _extra[1].Data.Where(dp => tank.SystemId == dp.TankSystemId).Select(dp => dp.Value).FirstOrDefault()) }
+            )).ToList();
+        }
+
+        private static IEnumerable<Tank> DistinctTanks(IEnumerable<Tank> tanks)
+        {
+            var tankList = tanks.ToList();
+            return tankList.Select(t => new { t.Category, t.Class, t.Country }).Distinct()
+                .Select(p => tankList.First(t => t.Category == p.Category && t.Class == p.Class && t.Country == p.Country));
         }
     }
 }
