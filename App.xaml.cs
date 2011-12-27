57eb3b38367dbd98fd4f0b22ae16008339bee44c
rstarkov <rstarkov@localhost>
2011-12-27 02:34:24 +0000

    Discover all the present icon makers and add them to the list of makers. Changing makers updates the icons preview. All settings are properly saved.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Linq;
 using System.Windows;
+using RT.Util;
 
 namespace TankIconMaker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public App() { Program.App = this; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            System.Windows.Forms.Application.EnableVisualStyles();
+            System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
+
+            base.OnStartup(e);
+            SettingsUtil.LoadSettings(out Program.Settings);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Program.Settings.SaveQuiet();
+            base.OnExit(e);
+        }
+    }
+
+    static class Program
+    {
+        public static App App;
+        public static Settings Settings;
     }
 }
---
+++ Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RT.Util;
+using RT.Util.Forms;
+
+namespace TankIconMaker
+{
+    [Settings("TankIconMaker", SettingsKind.UserSpecific)]
+    class Settings : SettingsThreadedBase
+    {
+        public ManagedWindow.Settings MainWindow = new ManagedWindow.Settings();
+
+        public string SelectedMakerType;
+        public string SelectedMakerName;
+
+        protected override SettingsThreadedBase CloneForSaveThreaded()
+        {
+            var result = (Settings) MemberwiseClone();
+            return result;
+        }
+    }
+}
