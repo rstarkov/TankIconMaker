e3a35e43d69d518d7eecd0f7bb3eecd2779041e6
rstarkov <rstarkov@localhost>
2012-01-15 20:57:12 +0000

    - Game paths can be added/deleted properly and version selected - Maker description more readable - Data fixes

--- Settings.cs
+++ Settings.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 using RT.Util;
-using RT.Util.Forms;
 
 namespace TankIconMaker
 {
@@ -21,7 +18,7 @@ namespace TankIconMaker
         public string SelectedGamePath = Ut.FindTanksDirectory();
 
         /// <summary>Settings that are specific to a game installation path.</summary>
-        public List<GameInstallationSettings> GameInstalls = new List<GameInstallationSettings>();
+        public ObservableSortedList<GameInstallationSettings> GameInstalls = new ObservableSortedList<GameInstallationSettings>();
 
         protected override SettingsThreadedBase CloneForSaveThreaded()
         {
@@ -30,16 +27,46 @@ namespace TankIconMaker
         }
     }
 
-    sealed class GameInstallationSettings
+    sealed class GameInstallationSettings : IComparable<GameInstallationSettings>, INotifyPropertyChanged
     {
         /// <summary>Absolute path to the root of this game installation.</summary>
-        public string Path { get; set; }
-        /// <summary>The version of the game that is located at this path.</summary>
-        public string GameVersion;
-
-        public override string ToString()
+        public string Path
         {
-            return "[{0}] {1}".Fmt(GameVersion, Path);
+            get { return _path; }
+            set
+            {
+                _path = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Path"));
+                PropertyChanged(this, new PropertyChangedEventArgs("DisplayName"));
+            }
         }
+        private string _path;
+
+        /// <summary>The version of the game that is located at this path.</summary>
+        public string GameVersion
+        {
+            get { return _gameVersion; }
+            set
+            {
+                _gameVersion = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("GameVersion"));
+                PropertyChanged(this, new PropertyChangedEventArgs("DisplayName"));
+            }
+        }
+        private string _gameVersion;
+
+        /// <summary>The value displayed in the drop-down.</summary>
+        public string DisplayName { get { return _gameVersion + ":  " + _path; } }
+
+        public int CompareTo(GameInstallationSettings other)
+        {
+            if (other == null) return 1;
+            int result = -string.Compare(GameVersion, other.GameVersion);
+            if (result != 0)
+                return result;
+            return string.Compare(Path, other.Path);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = (_, __) => { };
     }
 }
