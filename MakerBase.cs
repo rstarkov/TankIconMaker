28b90d80d1c59b315230a7fd06f1d916a2cffef5
rstarkov <rstarkov@localhost>
2012-01-26 12:46:44 +0000

    - proper handling and reporting of exceptions in the maker render method - LambdaBinding helper for creating single- or multi-bindings with lambdas for conversions - improved stack trace collapsing - misc

--- MakerBase.cs
+++ MakerBase.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -26,23 +27,6 @@ namespace TankIconMaker
         public override string ToString()
         {
             return "{0} by {1} (v{2})".Fmt(Name, Author, Version);
-        }
-    }
-
-    abstract class MakerBaseWpf : MakerBase
-    {
-        public abstract void DrawTank(Tank tank, DrawingContext dc);
-
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public override BitmapSource DrawTankInternal(Tank tank)
-        {
-            var visual = new DrawingVisual();
-            using (var context = visual.RenderOpen())
-                DrawTank(tank, context);
-            var bitmap = new RenderTargetBitmap(80, 24, 96, 96, PixelFormats.Pbgra32);
-            bitmap.Render(visual);
-            bitmap.Freeze();
-            return bitmap;
         }
 
         /// <summary>
@@ -81,6 +65,23 @@ namespace TankIconMaker
         }
     }
 
+    abstract class MakerBaseWpf : MakerBase
+    {
+        public abstract void DrawTank(Tank tank, DrawingContext dc);
+
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override BitmapSource DrawTankInternal(Tank tank)
+        {
+            var visual = new DrawingVisual();
+            using (var context = visual.RenderOpen())
+                DrawTank(tank, context);
+            var bitmap = new RenderTargetBitmap(80, 24, 96, 96, PixelFormats.Pbgra32);
+            bitmap.Render(visual);
+            bitmap.Freeze();
+            return bitmap;
+        }
+    }
+
     abstract class MakerBaseGdi : MakerBase
     {
         public abstract void DrawTank(Tank tank, D.Graphics dc);
@@ -93,5 +94,10 @@ namespace TankIconMaker
                 DrawTank(tank, g);
             return result.ToWpf();
         }
+    }
+
+    class MakerUserError : Exception
+    {
+        public MakerUserError(string message) : base(message) { }
     }
 }
