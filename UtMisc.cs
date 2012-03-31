1c9bc31bda1b57cdc67a8716a81923a46c3be1ff
Roman Starkov <sourcecode@roman.st>
2012-03-31 14:22:00 +0100

    Start using WpfCrutches.

--- UtMisc.cs
+++ UtMisc.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
 using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Win32;
 
@@ -279,81 +275,6 @@ namespace TankIconMaker
         }
     }
 
-    /// <summary>A crutch that enables a sensible way to bind to a dependency property with a custom conversion.</summary>
-    sealed class LambdaConverter<TSource, TResult> : IValueConverter
-    {
-        private Func<TSource, TResult> _lambda;
-        private Func<TResult, TSource> _lambdaBack;
-
-        public LambdaConverter(Func<TSource, TResult> lambda, Func<TResult, TSource> lambdaBack)
-        {
-            _lambda = lambda;
-            _lambdaBack = lambdaBack;
-        }
-
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (!targetType.IsAssignableFrom(typeof(TResult)))
-                throw new InvalidOperationException();
-            return _lambda((TSource) value);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (_lambdaBack == null)
-                throw new NotImplementedException();
-            if (!targetType.IsAssignableFrom(typeof(TSource)))
-                throw new InvalidOperationException();
-            return _lambdaBack((TResult) value);
-        }
-    }
-
-    /// <summary>A crutch that enables a sensible way to bind to several dependency properties with a custom conversion.</summary>
-    sealed class LambdaMultiConverter<T1, T2, TResult> : IMultiValueConverter
-    {
-        private Func<T1, T2, TResult> _lambda;
-
-        public LambdaMultiConverter(Func<T1, T2, TResult> lambda)
-        {
-            _lambda = lambda;
-        }
-
-        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values.Length != 2)
-                throw new InvalidOperationException();
-            if (!targetType.IsAssignableFrom(typeof(TResult)))
-                throw new InvalidOperationException();
-            return _lambda((T1) values[0], (T2) values[1]);
-        }
-
-        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    /// <summary>A crutch that enables a sensible way to bind to a dependency property with a custom conversion.</summary>
-    static class LambdaBinding
-    {
-        /// <summary>Returns the binding with a new converter, one that uses the specified lambda(s) to perform conversions.</summary>
-        public static Binding New<TSource, TResult>(Binding binding, Func<TSource, TResult> lambda, Func<TResult, TSource> lambdaBack = null)
-        {
-            binding.Converter = new LambdaConverter<TSource, TResult>(lambda, lambdaBack);
-            return binding;
-        }
-
-        /// <summary>Creates a new multi-binding consisting of two bindings, using the specified lambda for conversions.</summary>
-        public static MultiBinding New<T1, T2, TResult>(Binding b1, Binding b2, Func<T1, T2, TResult> lambda)
-        {
-            var result = new MultiBinding();
-            result.Bindings.Add(b1);
-            result.Bindings.Add(b2);
-            result.Converter = new LambdaMultiConverter<T1, T2, TResult>(lambda);
-            return result;
-        }
-    }
-
     /// <summary>
     /// Enables scheduling tasks to execute in a thread of a different priority than Normal. Only non-critical tasks
     /// should be scheduled using this scheduler because any remaining queued tasks will be dropped on program exit.
@@ -412,42 +333,5 @@ namespace TankIconMaker
         {
             return false; // we might not want to execute task that should schedule as high or low priority inline
         }
-    }
-
-
-    /// <summary>
-    /// Encapsulates a single value which dependency properties can depend on, and which can depend on other
-    /// dependency properties.
-    /// </summary>
-    public sealed class DependencyValue<T> : DependencyObject
-    {
-        public T Value
-        {
-            get { return (T) GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, value); }
-        }
-
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
-            "Value", typeof(T), typeof(DependencyValue<T>), new PropertyMetadata(default(T)));
-    }
-
-    /// <summary>
-    /// Encapsulates a single value which dependency properties can depend on.
-    /// </summary>
-    public sealed class ObservableValue<T> : INotifyPropertyChanged
-    {
-        private T _value;
-
-        public static implicit operator T(ObservableValue<T> value) { return value._value; }
-        public ObservableValue() { }
-        public ObservableValue(T value) { _value = value; }
-
-        public T Value
-        {
-            get { return _value; }
-            set { _value = value; PropertyChanged(this, new PropertyChangedEventArgs("Value")); }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged = (_, __) => { };
     }
 }
