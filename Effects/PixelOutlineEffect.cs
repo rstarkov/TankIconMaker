c36ced227853d6c5f02aa586a70a3d8fd14742d5
rstarkov <rstarkov@localhost>
2012-04-18 12:09:27 +0100

    - Added 1-pixel outline effect. Use it in the Black_Spy built-in style. - Version bump to v007

---
+++ Effects/PixelOutlineEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
+
+namespace TankIconMaker.Effects
+{
+    class PixelOutlineEffect : EffectBaseWpf
+    {
+        public override int Version { get { return 1; } }
+        public override string TypeName { get { return "Outline"; } }
+        public override string TypeDescription { get { return "Adds a 1 pixel outline around the image. Not suitable for layers with soft outlines."; } }
+
+        [Category("Outline")]
+        [Description("Specifies which color to use. Use the Alpha channel to adjust the strength of the effect.")]
+        [ExpandableObject]
+        public ColorSelector Color { get; set; }
+
+        public PixelOutlineEffect()
+        {
+            Color = new ColorSelector(Colors.Black);
+        }
+
+        public override WriteableBitmap Apply(Tank tank, WriteableBitmap layer)
+        {
+            return Ut.NewBitmapWpf(dc =>
+            {
+                dc.DrawImage(layer);
+                dc.DrawImage(layer.GetOutline(Color.GetColorWpf(tank)));
+            }).ToWpfWriteable();
+        }
+    }
+}
