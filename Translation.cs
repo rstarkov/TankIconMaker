6fafcfa5df2562938f86846b52b10698983455bf
rstarkov <rstarkov@localhost>
2012-08-02 15:37:27 +0100

    All controls directly present in the main window are now translatable.

--- Translation.cs
+++ Translation.cs
@@ -6,6 +6,12 @@ using RT.Util.Lingo;
 
 namespace TankIconMaker
 {
+    public enum TranslationGroup
+    {
+        [LingoGroup("Main window", "Contains strings used directly in the main window interface.")]
+        MainWindow,
+    }
+
     [LingoStringClass]
     sealed class Translation : TranslationBase
     {
