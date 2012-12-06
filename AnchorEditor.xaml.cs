1b508da071fb6993672ddf8aedbc9080db7db8aa
rstarkov <rstarkov@localhost>
2012-12-06 23:18:55 +0000

    - All text layers now have a nicer control for editing anchor settings (fixes T-10) - All text layers now support fitting to width/height (fixes T-5) - Demo style updated: added an invisible layer.

---
+++ AnchorEditor.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using WpfCrutches;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+using Xceed.Wpf.Toolkit.PropertyGrid.Editors;
+
+namespace TankIconMaker
+{
+    public partial class AnchorEditor : UserControl, ITypeEditor
+    {
+        private BindingExpression _expression;
+
+        public AnchorEditor()
+        {
+            InitializeComponent();
+        }
+
+        public FrameworkElement ResolveEditor(PropertyItem propertyItem)
+        {
+            bind(propertyItem, btnTopLeft, Anchor.TopLeft);
+            bind(propertyItem, btnTopCenter, Anchor.TopCenter);
+            bind(propertyItem, btnTopRight, Anchor.TopRight);
+            bind(propertyItem, btnMidLeft, Anchor.MidLeft);
+            bind(propertyItem, btnMidCenter, Anchor.MidCenter);
+            bind(propertyItem, btnMidRight, Anchor.MidRight);
+            bind(propertyItem, btnBottomLeft, Anchor.BottomLeft);
+            bind(propertyItem, btnBottomCenter, Anchor.BottomCenter);
+            bind(propertyItem, btnBottomRight, Anchor.BottomRight);
+            return this;
+        }
+
+        private void bind(PropertyItem propertyItem, RadioButton btnTopLeft, Anchor anchor)
+        {
+            BindingOperations.SetBinding(btnTopLeft, ToggleButton.IsCheckedProperty, LambdaBinding.New(
+                new Binding("Value") { Source = propertyItem, Mode = propertyItem.IsReadOnly ? BindingMode.OneWay : BindingMode.TwoWay },
+                (object source) => { return (Anchor) source == anchor; },
+                (bool source) => { return source ? anchor : Binding.DoNothing; }
+            ));
+        }
+
+    }
+}
