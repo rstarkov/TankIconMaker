832d1616a29862faf8911d499c426e62489fdc72
Roman Starkov <sourcecode@roman.st>
2015-03-12 22:33:11 +0000

    Hue is now specified as the more conventional shift in degrees. More tweaks to the translations. Some renames.

--- Effects/ModulateEffect.cs
+++ Effects/ModulateEffect.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.IO;
-using System.Windows.Media.Imaging;
 using ImageMagick;
 using RT.Util.Lingo;
 
@@ -9,37 +7,37 @@ namespace TankIconMaker.Effects
     class ModulateEffect : EffectBase
     {
         public override int Version { get { return 1; } }
-        public override string TypeName { get { return App.Translation.EffectHueSaturationBrightness.EffectName; } }
-        public override string TypeDescription { get { return App.Translation.EffectHueSaturationBrightness.EffectDescription; } }
+        public override string TypeName { get { return App.Translation.EffectHueSaturationLightness.EffectName; } }
+        public override string TypeDescription { get { return App.Translation.EffectHueSaturationLightness.EffectDescription; } }
 
-        public double Hue { get { return _Hue; } set { _Hue = Math.Min(200.0, Math.Max(0.0, value)); } }
+        public double Hue { get { return _Hue; } set { _Hue = Math.Min(360, Math.Max(-360, value)); } }
         private double _Hue;
-        public static MemberTr HueTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationBrightness.Hue); }
+        public static MemberTr HueTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationLightness.Hue); }
 
         public double Saturation { get { return _Saturation; } set { _Saturation = Math.Max(0.0, value); } }
         private double _Saturation;
-        public static MemberTr SaturationTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationBrightness.Saturation); }
+        public static MemberTr SaturationTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationLightness.Saturation); }
 
-        public double Brightness { get { return _Brightness; } set { _Brightness = Math.Max(0.0, value); } }
-        private double _Brightness;
-        public static MemberTr BrightnessTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationBrightness.Brightness); }
+        public double Lightness { get { return _Lightness; } set { _Lightness = Math.Max(0.0, value); } }
+        private double _Lightness;
+        public static MemberTr LightnessTr(Translation tr) { return new MemberTr(tr.Category.Settings, tr.EffectHueSaturationLightness.Lightness); }
 
         public ModulateEffect()
         {
-            _Hue = 100;
+            _Hue = 0;
             _Saturation = 100;
-            _Brightness = 100;
+            _Lightness = 100;
         }
 
         public override BitmapBase Apply(Tank tank, BitmapBase layer)
         {
-            if (Hue == 100 && Saturation == 100 && Brightness == 100)
+            if (Hue == 0 && Saturation == 100 && Lightness == 100)
                 return layer;
 
             using (var image = layer.ToMagickImage())
             {
                 image.BackgroundColor = MagickColor.Transparent;
-                image.Modulate(new Percentage(Brightness), new Percentage(Saturation), new Percentage(Hue));
+                image.Modulate(new Percentage(Lightness), new Percentage(Saturation), new Percentage(Hue * 100.0 / 180.0 + 100));
 
                 layer.CopyPixelsFrom(image.ToBitmapSource());
                 return layer;
