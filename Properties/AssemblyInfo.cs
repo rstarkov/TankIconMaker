6dad11c283e18687d3651fd05f4f4fbb2dd7493d
rstarkov <sourcecode@roman.st>
2015-03-16 16:38:23 +0000

    Version bump to v043.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("42.0.0.0")]
-[assembly: AssemblyFileVersion("42.0.0.0")]
+[assembly: AssemblyVersion("43.0.0.0")]
+[assembly: AssemblyFileVersion("43.0.0.0")]
