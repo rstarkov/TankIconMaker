bb4b74be90bfe249926292a3588263f01775b959
AstaRom <astafev.roman.mail@gmail.com>
2021-03-03 14:15:56 +0500

    Version bump to v063

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("62.0.0.0")]
-[assembly: AssemblyFileVersion("62.0.0.0")]
+[assembly: AssemblyVersion("63.0.0.0")]
+[assembly: AssemblyFileVersion("63.0.0.0")]
